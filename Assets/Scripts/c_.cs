29d92b81450006a20f9f83fbc19f1d02f6488465
Júlio César Cabral de Moura <juliocsar.cabral7@gmail.com>
2019-02-12 10:20:11 -0200

    "Programing with Animator"
    Good for prototypes.

---
+++ Assets/Scripts/c_.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_ : MonoBehaviour {
+
+    private bool liga;
+    
+    void Start() {
+
+        liga = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetButtonDown("Jump") && liga == false) {
+
+            liga = true;
+
+            gameObject.GetComponent<Animator>().Play("Rotate");
+        }
+        
+        if (Input.GetButtonDown("Jump") && liga == true) {
+
+            liga = false;
+
+            gameObject.GetComponent<Animator>().Play("Off");
+        }
+	}
+}
